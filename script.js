2c4fbcb1682b022b0d15ab430c25e58ecb6d3671
Андрей Колесников <kolesnikov.andrey@outlook.com>
2025-06-11 17:39:17 +0500

    Added styles and scripts

--- script.js
+++ script.js
@@ -236,5 +236,5 @@ document.getElementById('logout-admin').addEventListener('click', () => {
 
 // Функционал кнопки справка
 document.getElementById('helpButton').onclick = function() {
-  window.open('https://disk.yandex.ru/d/-hNkZ9EVyGhm-Q', '_blank');
+  window.open('https://disk.360.yandex.ru/i/BsFF0GbuZjBlmw', '_blank');
 };
